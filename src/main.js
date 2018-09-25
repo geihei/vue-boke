e6e1dd46dfb58b2617338f8f23376c7a6f745c31
geihei <1115414944@qq.com>
2018-09-25 14:27:13 +0800

    引入elementui

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './network/index'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 Vue.use(api)
+
 new Vue({
     el: '#app',
     router,
